07f58c245162c8b1e8c9c893867359395b3304dd
Kevin Thai <kevin.k.thai@gmail.com>
2020-01-28 22:01:51 -0600

    Rules given a hide option

--- client/src/components/ClueBoard.js
+++ client/src/components/ClueBoard.js
@@ -1,61 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Spinner, ListGroup, ListGroupItem } from 'reactstrap'
 import ValidationForm from './ValidationForm'
 import ClueForm from './ClueForm'
 import Results from './Results'
 
-class ClueBoard extends React.Component {
-  render() {
-    return this.props.G.result ? (
-      <Results {...this.props} playerID={this.props.playerID} />
-    ) : (
+const ClueBoard = props => {
+  return props.G.result ? (
+    <Results {...props} playerID={props.playerID} />
+  ) : (
+    <div>
+      <h2>{props.stage ? props.G.stage[props.stage] : 'Waiting...'}</h2>
       <div>
-        <h2>{this.props.stage ? this.props.G.stage[this.props.stage] : 'Waiting...'}</h2>
-        <div>
-          {this.props.G.currentWord ? (
-            <h3>
-              The mystery word is: <span className="emphasis">{this.props.G.currentWord}</span>
-            </h3>
-          ) : (
-            ''
-          )}
-        </div>
-        {this.props.stage === 'clue' ? <ClueForm {...this.props} /> : ''}
-        {this.props.stage === 'validate' ? <ValidationForm {...this.props} /> : ''}
-        {this.props.stage !== 'clue' &&
-        this.props.stage !== 'validate' &&
-        !Object.values(this.props.ctx.activePlayers).includes('guess') ? (
-          <h4>
-            <Spinner color="primary" /> Waiting for the other clue givers...
-          </h4>
-        ) : (
-          ''
-        )}
-        {Object.values(this.props.ctx.activePlayers).includes('guess') ? (
-          <div>
-            <h4>
-              <Spinner color="primary" /> Validations submitted! Waiting for the guesser...
-            </h4>
-            <hr />
-            <h4>The valid clues are:</h4>
-            <ListGroup>
-              {Object.keys(this.props.G.clues).map((clue, i) =>
-                this.props.G.clues[clue] > 0 ? (
-                  <ListGroupItem key={i} className="emphasis">
-                    {clue}
-                  </ListGroupItem>
-                ) : (
-                  ''
-                )
-              )}
-            </ListGroup>
-          </div>
+        {props.G.currentWord ? (
+          <h3>
+            The mystery word is: <span className="emphasis">{props.G.currentWord}</span>
+          </h3>
         ) : (
           ''
         )}
       </div>
-    )
-  }
+      {props.stage === 'clue' ? <ClueForm {...props} /> : ''}
+      {props.stage === 'validate' ? <ValidationForm {...props} /> : ''}
+      {props.stage !== 'clue' &&
+      props.stage !== 'validate' &&
+      !Object.values(props.ctx.activePlayers).includes('guess') ? (
+        <h4>
+          <Spinner color="primary" /> Waiting for the other clue givers...
+        </h4>
+      ) : (
+        ''
+      )}
+      {Object.values(props.ctx.activePlayers).includes('guess') ? (
+        <div>
+          <h4>
+            <Spinner color="primary" /> Validations submitted! Waiting for the guesser...
+          </h4>
+          <hr />
+          <h4>The valid clues are:</h4>
+          <ListGroup>
+            {Object.keys(props.G.clues).map((clue, i) =>
+              props.G.clues[clue] > 0 ? (
+                <ListGroupItem key={i} className="emphasis">
+                  {clue}
+                </ListGroupItem>
+              ) : (
+                ''
+              )
+            )}
+          </ListGroup>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  )
 }
 
 export default ClueBoard
--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -1,72 +1,83 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
-export default props => (
-  <div className="rListGroupes">
-    {props.stage === 'validate' ? (
-      <Fragment>
-        <h4>Validation rules:</h4>
-        <ListGroup>
-          <ListGroupItem>
-            Reject all clues that are identical or variants of the same word <br />
-            (i.e. plurals, misspellings, gender differentiations)
-          </ListGroupItem>
-          <ListGroupItem>
-            Reject all clues that are from the same word family <br />
-            (e.g. Prince and Princess)
-          </ListGroupItem>
-        </ListGroup>
-      </Fragment>
-    ) : (
-      ''
-    )}
-    <br />
-    <h4>Valid clues:</h4>
-    <ListGroup>
-      <ListGroupItem>
-        A digit or number <br />
-        (e.g. '1', '007', '7732025862' are valid clues)
-      </ListGroupItem>
-      <ListGroupItem>
-        An onomatopoeia <br />
-        (e.g. 'RIIIIIING' is a valid clue for the Mystery word 'PHONE')
-      </ListGroupItem>
-      <ListGroupItem>
-        An acronym <br />
-        (e.g. 'FBI' is a valid clue)
-      </ListGroupItem>
-      <ListGroupItem>
-        A special character <br />
-        (e.g. '$' is a valid clue for the Mystery word 'CAPITALISM')
-      </ListGroupItem>
-    </ListGroup>
-    <br />
-    <h4>Invalid clues:</h4>
-    <ListGroup>
-      <ListGroupItem>
-        More than one word <br />
-        (e.g. 'JONSNOW' is not a valid clue, but 'JON' or 'SNOW' are)
-      </ListGroupItem>
-      <ListGroupItem>
-        The Mystery word but spelled differently <br />
-        (e.g. 'SHURT' is not allowed if the mystery word is 'SHIRT')
-      </ListGroupItem>
-      <ListGroupItem>
-        The Mystery word in a foreign language <br />
-        (e.g. 'GRANDE' is not allowed if the mystery word is 'BIG')
-      </ListGroupItem>
-      <ListGroupItem>
-        A word from the same family as the Mystery word <br />
-        (e.g. 'PRINCESS' is not allowed if the mystery word is 'PRINCE')
-      </ListGroupItem>
-      <ListGroupItem>
-        A made up word <br />
-        (e.g. 'YUMYUMBREAD' is not a valid clue for 'CAKE', or any mystery word really)
-      </ListGroupItem>
-      <ListGroupItem>
-        A word that is phonetically identical to the Mystery word <br />
-        (e.g. 'WHETHER' is not a allowed if the mystery word is 'WEATHER')
-      </ListGroupItem>
-    </ListGroup>
-  </div>
-)
+export default props => {
+  const [hidden, setHidden] = useState(true)
+
+  const onHide = () => {
+    setHidden(!hidden)
+  }
+
+  return hidden ? (
+    <p onClick={onHide}>Click here to show the rules</p>
+  ) : (
+    <div className="rules">
+      <p onClick={onHide}>Click here to hide the rules</p>
+      {props.stage === 'validate' ? (
+        <Fragment>
+          <h4>Validation rules:</h4>
+          <ListGroup>
+            <ListGroupItem>
+              Reject all clues that are identical or variants of the same word <br />
+              (i.e. plurals, misspellings, gender differentiations)
+            </ListGroupItem>
+            <ListGroupItem>
+              Reject all clues that are from the same word family <br />
+              (e.g. Prince and Princess)
+            </ListGroupItem>
+          </ListGroup>
+          <br />
+        </Fragment>
+      ) : (
+        ''
+      )}
+      <h4>Valid clues:</h4>
+      <ListGroup>
+        <ListGroupItem>
+          A digit or number <br />
+          (e.g. '1', '007', '7732025862' are valid clues)
+        </ListGroupItem>
+        <ListGroupItem>
+          An onomatopoeia <br />
+          (e.g. 'RIIIIIING' is a valid clue for the Mystery word 'PHONE')
+        </ListGroupItem>
+        <ListGroupItem>
+          An acronym <br />
+          (e.g. 'FBI' is a valid clue)
+        </ListGroupItem>
+        <ListGroupItem>
+          A special character <br />
+          (e.g. '$' is a valid clue for the Mystery word 'CAPITALISM')
+        </ListGroupItem>
+      </ListGroup>
+      <br />
+      <h4>Invalid clues:</h4>
+      <ListGroup>
+        <ListGroupItem>
+          More than one word <br />
+          (e.g. 'JONSNOW' is not a valid clue, but 'JON' or 'SNOW' are)
+        </ListGroupItem>
+        <ListGroupItem>
+          The Mystery word but spelled differently <br />
+          (e.g. 'SHURT' is not allowed if the mystery word is 'SHIRT')
+        </ListGroupItem>
+        <ListGroupItem>
+          The Mystery word in a foreign language <br />
+          (e.g. 'GRANDE' is not allowed if the mystery word is 'BIG')
+        </ListGroupItem>
+        <ListGroupItem>
+          A word from the same family as the Mystery word <br />
+          (e.g. 'PRINCESS' is not allowed if the mystery word is 'PRINCE')
+        </ListGroupItem>
+        <ListGroupItem>
+          A made up word <br />
+          (e.g. 'YUMYUMBREAD' is not a valid clue for 'CAKE', or any mystery word really)
+        </ListGroupItem>
+        <ListGroupItem>
+          A word that is phonetically identical to the Mystery word <br />
+          (e.g. 'WHETHER' is not a allowed if the mystery word is 'WEATHER')
+        </ListGroupItem>
+      </ListGroup>
+    </div>
+  )
+}
